e5782c916392aed3d6bc1e5d3a5181a687372455
@themaskott <themaskott@gmail.com>
2022-07-06 16:39:55 +0200

    update connect

--- simple_connect_pwn.py
+++ simple_connect_pwn.py
@@ -5,20 +5,10 @@ from pwn import *
 HOST = "chall.host.com"
 PORT = 1337
 
-def conn():
-    if args.REMOTE:
-        r = remote(HOST, PORT)
-    else:
-        r = process([exe.path])
-
-    return r
-
-
-
 def main():
-    global r
-    r = conn()
+    r = remote(HOST, PORT)
     r.send()
+    r.sendline()
     r.recv()
     r.recvuntil()
 
--- simple_connect_socket.py
+++ simple_connect_socket.py
@@ -2,16 +2,42 @@ import socket
 import time
 import struct
 
-def send_bytes(s, b):
-	s.send(b)
-	rep = s.recv()
-	return rep
-
 
 HOST = '127.0.0.1'
 PORT = 1337
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    cookie = struct.unpack('<Q', leak)
-    payload = struct.pack('<Q', ret))
+
+class Connect:
+	def __init__(self, host, port):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.connect((host, port))
+
+	def send_bytes(self, b):
+		self.s.send(str(b).encode() + b'\n')
+		rep = self.s.recv(1024)
+		return rep
+
+	def recv_until(self, patern):
+		rep = b''
+		while patern not in rep:
+			rep += self.s.recv(1024)
+			# stop condition
+			if b'FLAG' in rep:
+				return rep
+		return rep
+
+	def close(self):
+		self.s.close()
+
+
+def main():
+	s = Connect(HOST, PORT)
+	s.send_bytes("hello")
+
+
+
+if __name__ == "__main__":
+    main()
+
+# cookie = struct.unpack('<Q', leak)
+# payload = struct.pack('<Q', ret))
